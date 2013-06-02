1c2b6bca866d15df3586c857bb218fd4d249a565
TJ Holowaychuk <tj@vision-media.ca>
2013-06-02 14:10:36 -0700

    add some visiting and var lookup

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-//
-// # Rework Variant
-//
-// A new take on Rework CSS variables.
-//
-module.exports = function (opts) {
-  opts = opts || { namespace: true };
-  return function (style) {
-    var vars = {};
-    var x = 0;
-    style.rules.forEach(function (rule, i) {
-      rule.selectors.forEach(function (sel) {
-        if (sel.indexOf('$') === 0) {
-          rule.declarations.forEach(function (d) {
-            vars[(opts.namespace ? sel + '.' : '$') + d.property] = d.value;
-          });
-          delete(style.rules[i]);
-        }
-        else {
-          rule.declarations.forEach(function (d) {
-            for (var j in vars) {
-              d.value = d.value.replace(j, vars[j]);
-            }
-          });
-        }
-      });
-    });
-  };
+
+module.exports = function(map){
+  return function(style){
+    return new Variables(map).visit(style);
+  }
 };
 
+function Variables(map) {
+  this.map = map || {};
+  this.visit = this.visit.bind(this);
+}
+
+// TODO: make sure we don't substitute within strings, url() etc.
+Variables.prototype.sub = function(str){
+  var self = this;
+  return str.replace(/\$([-\w]+)/g, function(_, name){
+    return self.lookup(name);
+  });
+};
+
+Variables.prototype.lookup = function(name){
+  if (this.map.hasOwnProperty(name)) {
+    return this.map[name];
+  }
+
+  throw new Error('failed to lookup variable $' + name);
+};
+
+Variables.prototype.stylesheet = function(node){
+  node.rules.forEach(this.visit);
+};
+
+Variables.prototype.rule = function(node){
+  node.declarations.forEach(this.visit);
+};
+
+Variables.prototype.declaration = function(node){
+  node.value = this.sub(node.value);
+};
+
+Variables.prototype.visit = function(node){
+  this[node.type || 'stylesheet'](node);
+};
